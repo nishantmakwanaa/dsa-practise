29b50d5bff5b06fac6545c60ae4ddacab496ec97
Nishant Makwana <nishantmakwanacreations@gmail.com>
2024-12-06 09:53:05 +0530

    LL Implementation

---
+++ Main.java
@@ -0,0 +1,16 @@
+import src.LL;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        LL list = new LL();
+
+        list.add(12);
+
+        System.out.println("Linked List:");
+
+        list.display();
+
+    }
+}
---
+++ src/LL.java
@@ -0,0 +1,37 @@
+package src;
+
+public class LL {
+    private Node head;
+
+    private static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public void add(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+
+    public void display() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " -> ");
+            current = current.next;
+        }
+        System.out.println("Null");
+    }
+}
